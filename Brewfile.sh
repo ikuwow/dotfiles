a4e354ba7f1881112cdd41d44464bb218882d9ca
Ikuo Degawa <degawa@ikuwow.com>
2014-11-23 00:08:54 +0900

    Brewfileをひと通り記述

--- Brewfile.sh
+++ Brewfile.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
 
 
+if [ ! `which brew` ]; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
-BASICS="tree git bash trash wget tmux"
+export HOMEBREW_CASK_OPTS="--appdir=/Applications --caskroom=/usr/local/Caskroom"
+
+# echo "Updating Homebrew... "
+# brew update
+# echo "Done!"
+
+# echo -n "Upgrading Homebrew... "
+# brew update
+# echo "Done!"
+
+BASICS="vim brew-cask gcc pwgen tree git bash trash wget tmux"
+echo "Installing Basic packages... "
 brew install $BASICS
+ecno "Done!"
 
-RUBY="ruby-build rbenv"
+RUBY="ruby-build rbenv gem"
+echo "Installing Ruby packages... "
 brew install $RUBY
+echo "Done!"
+
+CHEF="knife-solo berkshelf"
+echo "Installing Chef packages... "
+gem install $CHEF
+echo "Done!"
+
+CASKS="bettertouchtool menumeters vlc rescuetime firefox chrome karabiner cyberduck
+    iterm2 dropbox virtualbox vagrant mysqlworkbench google-japanese-ime github"
+echo "Installing Cask packages... "
+brew cask install $CASKS
+echo "Done!"
+
+echo -n "Cleanup... "
+brew cleanup
+echo "DONE!"
