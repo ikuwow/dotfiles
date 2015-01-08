fe5c364d3bfd22022e0d881cb413404e1ccc15c4
Ikuo Degawa <degawa@ikuwow.com>
2015-01-08 22:56:26 +0900

    gitconfigの位置付けを修正

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -14,7 +14,7 @@ ln -is ~/dotfiles/gvimrc ~/.gvimrc
 ln -is ~/dotfiles/bash_profile ~/.bash_profile
 ln -is ~/dotfiles/bashrc ~/.bashrc
 ln -is ~/dotfiles/.gitignore ~/.gitignore
-ln -is ~/dotfiles/.gitconfig ~/.gitconfig
+ln -is ~/dotfiles/gitconfig ~/.gitconfig
 ln -ins ~/dotfiles/bin/ ~/bin
 ln -ins ~/dotfiles/karabiner_private.xml ~/Library/Application\ Support/Karabiner/private.xml
 
