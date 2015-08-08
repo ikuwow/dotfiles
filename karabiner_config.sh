b34687d5bfdb2fa01b7213a15dbdeddfb7c85f83
Ikuo Degawa <degawa@ikuwow.com>
2015-08-08 18:09:37 +0900

    Remove hjkl cursor

--- karabiner_config.sh
+++ karabiner_config.sh
@@ -2,8 +2,6 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set remap.f5_f7_to_musiccontrols 1
-/bin/echo -n .
 $cli set remap.absolute_ctrl_j_to_return 1
 /bin/echo -n .
 /bin/echo
