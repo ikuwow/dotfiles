76995762b8cff58498fc7190c7b13f0a47296e2c
Ikuo Degawa <degawa@ikuwow.com>
2016-03-13 16:23:14 +0900

    add sshrc configuration

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-# dotfilesのシンボリックリンクを作成するスクリプト
 # A shellscript that creates symlinks of dotfiles
 
 if [ ! -e ~/dotfiles ]; then
     echo 'Error: A directory named "dotfiles" must be on your home directory.'
     exit 1
 fi
-
-# if [ ! -L ~/Library/Application\ Support/Karabiner/private.xml ]; then
-#     ln -is ~/dotfiles/karabiner_private.xml ~/Library/Application\ Support/Karabiner/private.xml
-# fi
-# 
-# if [ -d ~/.matlab/ -a ! -L ~/.matlab/R2014b/mexopts.sh ]; then
-#     ln -is ~/dotfiles/mexopts.sh ~/.matlab/R2014b/mexopts.sh
-# fi
 
 
 if [ ! -e ~/.vimrc ]; then
@@ -35,6 +26,14 @@ fi
 
 if [ ! -e ~/.gvimrc ]; then
     ln -is ~/dotfiles/gvimrc ~/.gvimrc
+fi
+
+if [ ! -e ~/.sshrc ]; then
+    ln -is ~/dotfiles/sshrc ~/.sshrc
+fi
+
+if [ ! -e ~/.sshrc.d ]; then
+    ln -is ~/dotfiles/sshrc.d ~/.sshrc.d
 fi
 
 if [ ! -e ~/.tmux.conf ]; then
