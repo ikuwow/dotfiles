e7fa0992806f1ec9b8cdcf617b4ec30e98c71b3d
Ikuo Degawa <degawa@ikuwow.com>
2016-09-18 10:52:48 +0900

    convert lightline font creation to script

--- lightline-font-logs.sh
+++ lightline-font-logs.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 
 git clone git@github.com:Lokaltog/vim-powerline.git
+cd vim-powerline/fontpatcher
 fontforge -script ./fontpatcher /Library/Fonts/OsakaMono.ttf
 cp OsakaMono-Powerline.ttf ~/Library/Fonts/
