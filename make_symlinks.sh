eb86dad68d378b589c9a25d2df60126068158bf4
Ikuo Degawa <degawa@ikuwow.com>
2018-09-10 00:21:41 +0900

    Refactor make symlinks

--- make_symlinks.sh
+++ make_symlinks.sh
@@ -7,31 +7,25 @@ if [ ! -e ~/dotfiles ]; then
     exit 1
 fi
 
-RCFILES=(\
-    .vimrc .gvimrc .vim .bash_profile .bashrc .macrc .sshrc .sshrc.d .tmux.conf .eslintrc .gemrc .atom .inputrc .gitignore
+RCFILES=(
+    .vimrc .gvimrc .vim .bash_profile .bashrc .macrc .sshrc .sshrc.d
+    .tmux.conf .eslintrc .gemrc .atom .inputrc .gitignore .gitconfig
 )
 for file in ${RCFILES[@]}; do
-    if [ ! -e ~/$file ]; then
-        ln -is ~/dotfiles/$file ~/$file
-    fi
+    ln -fhvs ~/dotfiles/$file ~/$file
 done
-
-if [ ! -e ~/.gitconfig ]; then
-    ln -is ~/dotfiles/.gitconfig ~/.gitconfig
-fi
+ln -fhvs ~/dotfiles/.vimrc ~/.ideavimrc
 
 # iCloud
 mkdir -p ~/iCloud\ Drive
-
 DIRS=(
   Keynote Numbers Pages Automator Notes
   Preview TextEdit QuickTimePlayerX ScriptEditor2
 )
 for dir in ${DIRS[@]}; do
-    ln -Ffhvs ~/Library/Mobile\ Documents/com~apple~${dir}/Documents ~/iCloud\ Drive/${dir}
+    ln -fhvs ~/Library/Mobile\ Documents/com~apple~${dir}/Documents ~/iCloud\ Drive/${dir}
 done
-
-ln -Ffhvs ~/Library/Mobile\ Documents/com~apple~CloudDocs ~/iCloud\ Drive/CloudDocs
+ln -fhvs ~/Library/Mobile\ Documents/com~apple~CloudDocs ~/iCloud\ Drive/CloudDocs
 
 if [ ! -e ~/.ssh/config ]; then
     ln -is ~/iCloud\ Drive/CloudDocs/ssh/config ~/.ssh/config
@@ -39,22 +33,8 @@ fi
 if [ ! -e ~/.ssh/config.d ]; then
     ln -is ~/iCloud\ Drive/CloudDocs/ssh/config.d ~/.ssh/config.d
 fi
-if [ ! -e ~/.ideavimrc ]; then
-    ln -is ~/dotfiles/.vimrc ~/.ideavimrc
-fi
 
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
-
-# NeoVim
-if [ ! -d $XDG_CONFIG_HOME/nvim ]; then
-    ln -is ~/.vim $XDG_CONFIG_HOME/nvim
-fi
-if [ ! -e $XDG_CONFIG_HOME/nvim/init.vim ]; then
-    ln -is ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
-fi
-
-# Karabiner Elements
-if [ ! -e $XDG_CONFIG_HOME/karabiner ]; then
-    ln -ihs ~/Dropbox/dotconfig/karabiner $XDG_CONFIG_HOME/karabiner
-fi
-
+ln -fhvs ~/.vim $XDG_CONFIG_HOME/nvim
+ln -fhvs ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+ln -fhvs ~/Dropbox/dotconfig/karabiner $XDG_CONFIG_HOME/karabiner
