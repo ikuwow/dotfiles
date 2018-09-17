6c76dc39ebf157e40a94238e937ac696c2c80f61
Ikuo Degawa <degawa@ikuwow.com>
2018-09-17 18:59:02 +0900

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+set -eu
+
+DOTPATH=$HOME/dotfiles
+
+if [ ! -d "$DOTPATH" ]; then
+    git clone https://github.com/ikuwow/dotfiles.git "$DOTPATH"
+else
+    echo "$DOTPATH already downloaded. Updating..."
+    cd "$DOTPATH"
+    git stash
+    git checkout master
+    git pull origin master
+    echo
+fi
+
+cd "$DOTPATH"
+
+# Mac basic settings
+scripts/configure.sh
+echo
+
+scripts/deploy.sh
+echo
+
+# install homebrew
+if ! command -v brew > /dev/null 2>&1; then
+    # Install homebrew: https://brew.sh/
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    echo
+fi
+brew bundle
+echo
+
+mackup restore
+echo
+
+scripts/initialize.sh
+echo
+
+echo "Bootstrapping DONE!"
