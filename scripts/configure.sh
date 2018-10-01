3c9b0e88cf08ab42cea82808eb4099d5a59bb2d1
Ikuo Degawa <degawa@ikuwow.com>
2018-10-01 18:31:12 +0900

    Fix order

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -18,29 +18,8 @@ defaults write com.apple.CrashReporter UseUNC -bool true
 defaults write KeyRepeat -int 2
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
-
-defaults write com.apple.systemuiserver menuExtras -array \
-    "/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
-    "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
-    "/System/Library/CoreServices/Menu Extras/TextInput.menu" \
-    "/System/Library/CoreServices/Menu Extras/Clock.menu" \
-    "/System/Library/CoreServices/Menu Extras/AirPort.menu" \
-    "/System/Library/CoreServices/Menu Extras/Eject.menu"
-
 # spotlight shortcut command + space
 defaults write com.apple.symbolichotkeys AppleSymbolicHotKeys -dict-add 64 "<dict><key>enabled</key><true/><key>value</key><dict><key>parameters</key><array><integer>65535</integer><integer>49</integer><integer>262144</integer></array><key>type</key><string>standard</string></dict></dict>"
-
-echo "Configuring Dock..."
-defaults write com.apple.dock show-recents -bool false
-defaults write com.apple.dock tilesize -int 77
-defaults write com.apple.dock largesize -int 91
-defaults write com.apple.dock magnification -int 1
-defaults write com.apple.dock wvous-bl-corner -int 11 # Launchpad
-defaults write com.apple.dock wvous-bl-modifier -int 0
-defaults write com.apple.dock wvous-br-corner -int 10 # Put display to sleep
-defaults write com.apple.dock wvous-br-modifier -int 0
-defaults write com.apple.dock wvous-tr-corner -int 12 # Notification Center
-defaults write com.apple.dock wvous-tr-modifier -int 0
 
 echo "Configuring Mouse..."
 defaults write com.apple.AppleMultitouchMouse MouseButtonDivision -int 55
@@ -61,23 +40,43 @@ echo "Configuring Keyboard..."
 defaults write NSGlobalDomain InitialKeyRepeat -int 35
 defaults write NSGlobalDomain KeyRepeat -int 2
 
+echo "Configuring SystemUIServer..."
+defaults write com.apple.systemuiserver menuExtras -array \
+    "/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
+    "/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
+    "/System/Library/CoreServices/Menu Extras/TextInput.menu" \
+    "/System/Library/CoreServices/Menu Extras/Clock.menu" \
+    "/System/Library/CoreServices/Menu Extras/AirPort.menu" \
+    "/System/Library/CoreServices/Menu Extras/Eject.menu"
+killall SystemUIServer
+
+echo "Configuring Dock..."
+defaults write com.apple.dock show-recents -bool false
+defaults write com.apple.dock tilesize -int 77
+defaults write com.apple.dock largesize -int 91
+defaults write com.apple.dock magnification -int 1
+defaults write com.apple.dock wvous-bl-corner -int 11 # Launchpad
+defaults write com.apple.dock wvous-bl-modifier -int 0
+defaults write com.apple.dock wvous-br-corner -int 10 # Put display to sleep
+defaults write com.apple.dock wvous-br-modifier -int 0
+defaults write com.apple.dock wvous-tr-corner -int 12 # Notification Center
+defaults write com.apple.dock wvous-tr-modifier -int 0
+killall Dock
+
 echo "Configuring Finder..."
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowStatusBar -bool true
 /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
 defaults write com.apple.finder FXRemoveOldTrashItems -bool true
+killall Finder
 
 echo "Configuring Safari..."
 defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 defaults write com.apple.Safari ShowStatusBar -bool true
 defaults write com.apple.Safari AutoFillPasswords -bool true
+killall Safari
 
-echo "Killing affected applications..."
-affectedApps=(Safari Finder Dock SystemUIServer)
-for app in "${affectedApps[@]}"; do
-    killall "$app"
-done
 
 echo ""
 echo "Configuration Complete!"
