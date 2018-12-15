5bf0e255a2400b8414f912957e21cccd3917029b
Ikuo Degawa <degawa@ikuwow.com>
2018-12-15 22:18:56 +0900

    Install kyrat

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -16,5 +16,17 @@ gem install neovim
 npm i -g neovim
 
 cp "$SCRIPT_DIR/brew_cleanup.plist" ~/Library/LaunchAgents/
-
 launchctl load "$SCRIPT_DIR/brew_cleanup.plist"
+
+KYRAT_PATH=~/.local/share/kyrat
+mkdir -p "$(dirname ~/.local/share)"
+if [ ! -d $KYRAT_PATH ]; then
+    git clone git@github.com:fsquillace/kyrat.git $KYRAT_PATH
+else
+    cd $KYRAT_PATH
+    git stash save
+    git checkout master
+    git pull origin master
+fi
+mkdir -p ~/bin
+ln -s "$KYRAT_PATH/bin/kyrat" ~/bin/kyrat
