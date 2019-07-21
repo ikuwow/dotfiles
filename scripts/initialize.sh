cc19aba4117eec569d08be86c035db8402dc0b2c
Ikuo Degawa <degawa@ikuwow.com>
2019-07-21 13:34:55 +0900

    Fix kyrat initialization

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -13,15 +13,10 @@ set -e
 /usr/local/bin/npm i -g npm
 /usr/local/bin/npm i -g neovim
 
-KYRAT_PATH=~/.local/share/kyrat
-mkdir -p "$(dirname ~/.local/share)"
-if [ ! -d $KYRAT_PATH ]; then
-    git clone git@github.com:fsquillace/kyrat.git $KYRAT_PATH
-else
-    cd $KYRAT_PATH
-    git stash save
-    git checkout master
-    git pull origin master
+[[ -z $XDG_DATA_HOME ]] && XDG_DATA_HOME=$HOME/.local/share
+mkdir -p "$XDG_DATA_HOME"
+if [ ! -d "$XDG_DATA_HOME/kyrat" ]; then
+    git clone git@github.com:fsquillace/kyrat.git "$XDG_DATA_HOME/kyrat"
 fi
 mkdir -p ~/bin
-ln -fs "$KYRAT_PATH/bin/kyrat" ~/bin/kyrat
+ln -fs "$XDG_DATA_HOME/bin/kyrat" ~/bin/kyrat
