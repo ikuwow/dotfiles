ae430e5c131e8bc7125a7e089a49ddd9cf62f670
Ikuo Degawa <degawa@ikuwow.com>
2020-04-26 16:09:55 +0900

    Fix kyrat url

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -15,7 +15,7 @@ brew install python
 [[ -z $XDG_DATA_HOME ]] && XDG_DATA_HOME=$HOME/.local/share
 mkdir -p "$XDG_DATA_HOME"
 if [ ! -d "$XDG_DATA_HOME/kyrat" ]; then
-  git clone git@github.com:fsquillace/kyrat.git "$XDG_DATA_HOME/kyrat"
+  git clone https://github.com/fsquillace/kyrat.git "$XDG_DATA_HOME/kyrat"
 fi
 mkdir -p ~/bin
 ln -fs "$XDG_DATA_HOME/kyrat/bin/kyrat" ~/bin/kyrat
