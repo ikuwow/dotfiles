bdfe283c6b97dea7e40f8b46809d4fa02e3aa4d5
Ikuo Degawa <degawa@ikuwow.com>
2020-12-20 12:53:30 +0900

    Install brew as Intel compatible

--- bootstrap/main.sh
+++ bootstrap/main.sh
@@ -10,9 +10,13 @@ echo
 
 # install homebrew
 if ! command -v brew >/dev/null 2>&1; then
+  prefix=""
+  if [ "$(uname -m)" = "arm64" ]; then
+    # Install on Rosetta 2
+    prefix="arch -arch x86_64"
+  fi
   # Install homebrew: https://brew.sh/
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  echo
+  "${prefix}" /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 brew bundle
 echo
