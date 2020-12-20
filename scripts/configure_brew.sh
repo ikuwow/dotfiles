27499c6ee648742be9eac1300fe3936381e7886a
Ikuo Degawa <degawa@ikuwow.com>
2020-12-20 21:30:59 +0900

    Check before enabling brew autoupdate

--- scripts/configure_brew.sh
+++ scripts/configure_brew.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 echo "Configuring brew autoupdate..."
-brew autoupdate --upgrade --cleanup --enable-notificationn --start
+if brew autoupdate --status | grep -v 'Autoupdate is installed and running.' > /dev/null; then
+  brew autoupdate --upgrade --cleanup --enable-notification --start
+else
+  echo "Autoupdate is enabled. Nothing to do."
+fi
