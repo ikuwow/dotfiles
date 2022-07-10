95fc51bc4a44e73a073e39e07bd3a7ee302f83b9
Ikuo Degawa <degawa@ikuwow.com>
2022-07-10 21:55:09 +0900

    Remove command prefix

--- bootstrap/main.sh
+++ bootstrap/main.sh
@@ -26,35 +26,24 @@ echo
 scripts/deploy.sh
 echo
 
-command_prefix=""
-if [ "$(uname -m)" = "arm64" ]; then
-  # Install on Rosetta 2
-  command_prefix="arch -arch x86_64"
-fi
-
 # Install Rosetta 2 when ARM
 if [ "$(uname -m)" = "arm64" ]; then
   softwareupdate --install-rosetta --agree-to-license
 fi
 
 # Install homebrew for Intel
-if ! command -v /usr/local/bin/brew >/dev/null 2>&1; then
+if ! command -v brew > /dev/null 2>&1; then
   # Install homebrew: https://brew.sh/
-  $command_prefix /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-fi
-
-# Install homebrew for ARM
-if [ "$(uname -m)" = "arm64" ] && (! command -v /opt/homebrew/bin/brew > /dev/null); then
-  sudo mkdir /opt/homebrew
-  sudo chown "$(whoami)" /opt/homebrew
-  curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C /opt/homebrew
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+  echo "Homebrew is already installed."
 fi
 
 # TODO: prioritize ARM homebrew
 # export PATH="/opt/homebrew/bin:/usr/local/bin:$PATH"
 
 export HOMEBREW_BUNDLE_NO_LOCK=1
-$command_prefix brew bundle
+brew bundle
 echo
 
 mackup restore
