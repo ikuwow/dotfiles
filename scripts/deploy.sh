18caa727dde0ea0ebe499dd8008f5a63795351d0
Ikuo Degawa <ikuwow@gmail.com>
2025-06-20 19:11:03 +0900

    Deploy AIRULES.md to CLAUDE.md

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -42,3 +42,7 @@ find "$DOTPATH/bin/" -type f -perm 0755 -exec ln -fvns {} ~/bin/ \;
 ICLOUD_DIR="$HOME/Library/Mobile Documents/com~apple~CloudDocs"
 [[ -d "$ICLOUD_DIR" ]] && ln -fvns "$ICLOUD_DIR" "$HOME/iCloudDrive"
 echo
+
+# AIRULES.md
+# https://docs.anthropic.com/en/docs/claude-code/memory#how-claude-looks-up-memories
+[[ -d "$HOME/.claude/" ]] && ln -fvns "$DOTPATH/AIRULES.md" "$HOME/.claude/CLAUDE.md"
