ab0b1dbab015b3606ffa1167c818afabceb1b62d
Ikuo Degawa <ikuwow@gmail.com>
2025-07-09 12:11:47 +0900

    Autoupdateの頻度を下げる

--- scripts/configure_brew.sh
+++ scripts/configure_brew.sh
@@ -4,7 +4,7 @@ echo "Configuring brew autoupdate..."
 brew autoupdate --status | grep 'Autoupdate is installed and running.' > /dev/null
 status="$?"
 if [ "$status" != 0 ]; then
-  brew autoupdate --start --upgrade --cleanup
+  brew autoupdate 259200 --start --upgrade --cleanup
 else
   echo "Autoupdate is enabled. Nothing to do."
 fi
